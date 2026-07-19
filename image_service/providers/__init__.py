9826b83dbf168784758e89515ed36d9f97ac307e
gpojarvis-sudo <gpojarvis@gmail.com>
2026-07-19 10:23:47 +0530

    Update __init__.py

--- image_service/providers/__init__.py
+++ image_service/providers/__init__.py
@@ -1,1 +1,11 @@
+from .base import BaseImageProvider
+from .huggingface import HuggingFaceProvider
+from .nvidia import NvidiaProvider
+from .manager import ImageProviderManager
 
+__all__ = [
+    "BaseImageProvider",
+    "HuggingFaceProvider",
+    "NvidiaProvider",
+    "ImageProviderManager",
+]
