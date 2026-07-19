33f1c5737812df91be3e37daa6958d43f2efd63e
gpojarvis-sudo <gpojarvis@gmail.com>
2026-07-19 08:41:07 +0530

    Create configuration package

---
+++ config/__init__.py
@@ -0,0 +1,12 @@
+"""
+Configuration Package
+
+This package contains all application
+configuration and environment settings.
+"""
+
+from .settings import settings
+
+__all__ = [
+    "settings",
+]
