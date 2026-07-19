746929ccc9fec5492ca7541f6e3dd891c0e32cba
gpojarvis-sudo <gpojarvis@gmail.com>
2026-07-19 09:28:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-from api.server import app
+import traceback
+
+try:
+    from api.server import app
+    print("SUCCESS: api.server imported successfully")
+except Exception:
+    print(traceback.format_exc())
+    raise
